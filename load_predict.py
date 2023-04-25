2622e8d75ecfceb437dac1d787515d5ec360ef55
sanjeeb tiwary <63203112+sanjeebtiwary@users.noreply.github.com>
2023-04-25 09:07:02 +0530

    load_predict.py

--- load_predict.py
+++ load_predict.py
@@ -1,28 +1,46 @@
-import os
-import torch
-import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
-import pickle
+import h5py
+import numpy as np
 
-# Load the trained model
-iris_model = nn.Sequential(
-            nn.Linear(4,32),
-            nn.Tanh(),
-            nn.Linear(32,3),
-            nn.LogSoftmax(dim=1))
+# Load the trained model from the model.h5 file
+with h5py.File('model.h5', 'r') as f:
+    coef = f['coef'][:]
+    intercept = f['intercept'][:]
 
-# iris_model.load_state_dict(torch.load('/kaggle/input/iris-pymodel/iris_model.pt'))
 
-iris_model.load_state_dict(torch.load("C:\\Users\\HOME\\Zummit_2023\\model_template.pt"))
+# Define a function to preprocess new text data and make predictions using the trained model
+def predict(text, cv, clf):
+    # Preprocess the text data using the same CountVectorizer object used to train the model
+    text_features = cv.transform([text]).toarray()
 
-iris_model.eval() # переводим модель в режим применения
-input_tensor = torch.tensor([[4.9,2.4,3.3,1.0]])
+    # Make predictions using the trained model
+    prediction = clf.predict(text_features)
 
-# Predictions
-outputs = iris_model(input_tensor)
-outputs = iris_model(input_tensor)
+    # Return the predicted label as a string
+    return target_names[prediction[0]]
 
-_, predicted = torch.max(outputs, dim=1)
 
-print('Output Tensor:\n', outputs)
-print('\nTorch Max Function Result: ', predicted)
+# Example usage
+from sklearn.feature_extraction.text import CountVectorizer
+
+# Load the CountVectorizer object used to train the model
+cv = CountVectorizer()
+
+# Fit the CountVectorizer object on the training data
+cv.fit(X_train)
+
+# Define the trained logistic regression classifier
+clf = LogisticRegression(random_state=7, max_iter=1000, solver='saga', n_jobs=4, multi_class='auto')
+
+# Set the learned coefficients and intercept values in the logistic regression classifier
+clf.coef_ = coef
+clf.intercept_ = intercept
+
+# Make predictions on new text data
+text1 = "This is a positive review"
+text2 = "This is a negative review"
+prediction1 = predict(text1, cv, clf)
+prediction2 = predict(text2, cv, clf)
+
+# Print the predictions
+print(prediction1)  # should output 'positive'
+print(prediction2)  # should output 'negative'
